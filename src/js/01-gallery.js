90df043f2c297a6d4124fc01fa91f5ba0adbaa71
Katarzyna Leoniuk <kasia.leoniuk@gmail.com>
2023-02-25 18:52:30 +0100

    Created: task 1 and task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+const galleryCard = createGalleryCards(galleryItems);
+galleryDiv.insertAdjacentHTML('afterbegin', galleryCard);
+
+function createGalleryCards(images) {
+  return images
+    .map(
+      image =>
+        `<a class="gallery__item" href="${image.original}" onclick="return false;">
+<img class="gallery__image" 
+src="${image.preview}" 
+alt="${image.description}" />
+</a>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  scrollZoomFactor: false,
+});
